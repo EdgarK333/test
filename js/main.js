d74141d16113a9dc91192294ffef332c220e5e70
EdgarK333 <102612771+EdgarK333@users.noreply.github.com>
2023-11-25 10:49:36 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const nav = document.querySelector('#nav');
 const navBtn = document.querySelector('#nav-button');
 const navBtnImg = document.querySelector('#nav-button-image');
+const navLinks = document.querySelectorAll('.nav-link');
 
 navBtn.onclick = () => {
   if (nav.classList.toggle('open')) {
@@ -9,5 +10,19 @@ navBtn.onclick = () => {
     navBtnImg.src = '/images/icons/nav-open.svg';
   }
 };
+
+// Добавляем обработчик события для каждой ссылки в навигационном меню
+navLinks.forEach((link) => {
+  link.addEventListener('click', () => {
+    // Закрываем навигационное меню
+    nav.classList.remove('open');
+    navBtnImg.src = '/images/icons/nav-open.svg';
+  });
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+  // Прокручиваем страницу вверх при загрузке
+  window.scrollTo(0, 0);
+});
 
 AOS.init();
